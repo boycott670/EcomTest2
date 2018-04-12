65ae3e23c9c288021bfa689d9b59ccc2797f0942
takingsss12 <takingsss12@gmail.com>
2018-04-12 10:25:19 +0100

    passed testBuyCapsules test

--- src/main/java/com/sqli/challenge/EcommerceFacade.java
+++ src/main/java/com/sqli/challenge/EcommerceFacade.java
@@ -1,5 +1,6 @@
 package com.sqli.challenge;
 
+import com.sqli.challenge.items.Capsule;
 import com.sqli.challenge.items.Cart;
 import com.sqli.challenge.items.Machine;
 import com.sqli.challenge.presenters.CartContentPresenter;
@@ -25,6 +26,11 @@ public final class EcommerceFacade
   public void removeMachine(final String name, final int quantity)
   {
     cart.addProduct(new Machine(name), -quantity);
+  }
+
+  public void addCapsule(final String name, final int quantity, final double price)
+  {
+    cart.addProduct(new Capsule(name, price), quantity);
   }
 
   public String cartContent()
